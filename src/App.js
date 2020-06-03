367d15e1be1f6956926a09cc38fff9f05a4e10f8
John Obertubbesing <oberthewhat@gmail.com>
2020-06-03 12:17:28 -0500

    added link tree

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 545,
-  },
-  media: {
-    height: 140,
-  },
-});
+import ReactTooltip from "react-tooltip";
 
 function App() {
-  const classes = useStyles();
-
-
-
 
   return (
-    <div>
+    <div className="root">
 
       <div className="navRoot">
         <div className="navPhoto">
@@ -39,17 +23,37 @@ function App() {
           </div>
         </div>
       </div>
+      <div className='linkBoxRoot'>
+        <div className="linkbox">
+          <a href="https://www.linkedin.com/in/john-obertubbesing-775b33185/" data-tip='LinkedIn' >
+            <i class="fab fa-linkedin"></i>
+          </a>
+
+          <a href="https://github.com/oberthewhat" data-tip="GitHub">
+            <i class="fab fa-github-square"></i>
+          </a>
+          <a href="https://www.facebook.com/oberthewhat" data-tip="Facebook">
+            <i class="fab fa-facebook-square"></i>
+          </a>
+          <a href="https://www.youtube.com/user/oberthewhat" data-tip='Youtube'>
+          <i class="fa fa-youtube-square" aria-hidden="true"></i>
+          </a>
+          <a href="mailto:oberthewhat@gmail.com" data-tip='Email'>
+          <i class="fa fa-envelope-square" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
       <div className='projectRoot'>
         <h2>Projects</h2>
         <div className='projectCard'>
           <Card className='cardRoot'>
             <CardActionArea>
               <a href='https://www.restaurantlistr.com'>
-              <CardMedia
-                className='cardMedia'
-                image="https://i.imgur.com/GeDZS6u.png"
-                title="Contemplative Reptile"
-              /></a>
+                <CardMedia
+                  className='cardMedia'
+                  image="https://i.imgur.com/GeDZS6u.png"
+                  title="Contemplative Reptile"
+                /></a>
 
             </CardActionArea>
             <CardContent>
@@ -57,17 +61,17 @@ function App() {
                 Listr
           </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                  This Page was made as my capstone project for school. This was a full stack program made with react, nodeJS, mySQL, Express, and lots of custom CSS, along with some use of material UI</Typography>
+                This Page was made as my capstone project for school. This was a full stack program made with react, nodeJS, mySQL, Express, and lots of custom CSS, along with some use of material UI</Typography>
             </CardContent>
           </Card>
           <Card className='cardRoot'>
             <CardActionArea>
               <a href='https://oberthewhat.github.io/ACA_web101_John_obertubbesing/src/pineshockey/index.html'>
-              <CardMedia
-                className='cardMedia'
-                image="https://i.imgur.com/yz9TjJ3.png"
-                title="Contemplative Reptile"
-              /></a>
+                <CardMedia
+                  className='cardMedia'
+                  image="https://i.imgur.com/yz9TjJ3.png"
+                  title="Contemplative Reptile"
+                /></a>
 
             </CardActionArea>
             <CardContent>
@@ -75,7 +79,8 @@ function App() {
                 Pinus Hockey
           </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                  This Page was made as my capstone project for school. This was a full stack program made with react, nodeJS, mySQL, Express, and lots of custom CSS, along with some use of material UI</Typography>
+                Using CSS and HTML i put togethere this page as a fun project for my beer leauge hockey team. There is also a small Javascript click game in the events section. </Typography>
+                <ReactTooltip />
             </CardContent>
           </Card>
         </div>
